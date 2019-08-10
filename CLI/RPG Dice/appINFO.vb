3c1de866d0fc7914256c615d3746b26049bdfd7e
Stephen-Baxter <52463998+Stephen-Baxter@users.noreply.github.com>
2019-08-10 15:23:43 -0500

    Create appINFO.vb

---
+++ CLI/RPG Dice/appINFO.vb
@@ -0,0 +1,10 @@
+'*****************************************************************************
+'Author: Stephen Baxter
+'Development Tool Used: Visual Studio 2019
+'Operating System Used: Windows 8.1
+'Name: RPG Dice
+'Language: Visual Basic
+'
+'Description: RPG Dice is a command line interface program to be use
+'in place of a physical four, six, eight, ten, twelve, or twenty sided Die.
+'*****************************************************************************
